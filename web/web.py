72a197e9dda3d483640234c31d18a91e8a67047e
Hrvoje <hrvoje.bresic@gmail.com>
2022-03-31 11:38:30 +0200

    added all to home page

--- web/web.py
+++ web/web.py
@@ -7,18 +7,6 @@ bp = Blueprint('web', __name__)
 def home():
     return render_template('home.html')
 
-@bp.route('/about')
-def about():
-    return render_template('about.html')
-
-@bp.route('/contact')
-def contact():
-    return render_template('contact.html')
-
-@bp.route('/send-inquiry', methods=['POST'])
-def send_inquiry():
-    pass
-
 @bp.app_errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html'), 404
